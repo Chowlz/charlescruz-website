e25411275ca544eec159822a0a023884ef02e8a8
Charles Cruz <mail@charlescruz.dev>
2024-02-02 01:19:47 +0000

    [app/page.js] Add flex-grow
    This allows the footer to be set to the bottom of page, regardless of
    the zoom level.

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col bg-primary-950">
       <Navbar />
-      <div className="container z-10 mx-auto mt-24 px-12 py-4">
+      <div className="container z-10 mx-auto mt-24 grow px-12 py-4">
         <Hero />
         <AboutMe />
       </div>
